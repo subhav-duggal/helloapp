2b869b5a64a1b875ef89062a2d293115c00060a0
akash-gautam <akash.gautam@macrometa.co>
2019-04-01 11:32:43 +0530

    fix(release-helloapp.sh) updated helm commands to pass image tag

--- scripts/release-helloapp.sh
+++ scripts/release-helloapp.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 TAG=$1
-sed -i "s/tag:.*/tag: $TAG/" charts/helloapp/values.yaml
 echo "start tiller"
 export KUBECONFIG=$HOME/.kube/kubeconfig
 helm tiller start-ci
 export HELM_HOST=127.0.0.1:44134
 result=$(eval helm ls | grep helloapp) 
 if [ $? -ne "0" ]; then 
-   helm install --timeout 180 --name helloapp charts/helloapp
+   helm install --timeout 180 --name helloapp --set image.tag=$TAG charts/helloapp
 else 
-   helm upgrade --timeout 180 helloapp charts/helloapp
+   helm upgrade --timeout 180 helloapp --set image.tag=$TAG charts/helloapp
 fi
 echo "stop tiller"
 helm tiller stop 
